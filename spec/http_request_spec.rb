f5fc226d55fea34f64f96a1025677b49c34f7a9b
Dillan Pisavadia <dpisavadia20@gmail.com>
2017-03-07 15:07:30 +0000

    Completed tests for HTTP GET request

---
+++ spec/http_request_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+require "http_request"
+
+RSpec.describe HTTPRequest do
+  let(:url) { "http://www.example.org" }
+  subject(:http) { described_class.new(url) }
+
+  describe "#get" do
+    context "when a url is passed to the function" do
+      it "returns the DOM body" do
+        stub_request(:get, "http://www.example.org").
+          to_return(:body, "<p>Hello world!</p>")
+
+        expect(http.get).to eq("<p>Hello world!</p>")
+      end
+    end
+  end
+end
