7c8415661a4cfe7e1c6567cfc5d09cd7ee5a0032
Dillan Pisavadia <dpisavadia20@gmail.com>
2017-03-07 15:41:58 +0000

    Tests completed to extract links

---
+++ lib/dom_parse.rb
@@ -0,0 +1,13 @@
+require "nokogiri"
+
+class DomParse
+  attr_reader :body
+
+  def initialize(body)
+    @body = body
+  end
+
+  def extract_links
+    
+  end
+end
---
+++ spec/dom_parse_spec.rb
@@ -0,0 +1,29 @@
+require "spec_helper"
+require "dom_parse"
+
+RSpec.describe DomParse do
+  let(:seed_url) { "http://www.example.org" }
+
+  describe "#extract_links" do
+    context "when passed a HTML body" do
+      let(:body_links) { "<p><a href='http://www.example.org/foo'>More</a></p>
+      <p>Follow <a href='http://www.iana.org/'>official here</a>.</p>" }
+      subject(:parser) { described_class.new(seed_url, body_links) }
+
+      it "returns the links in the body" do
+        expect(parser.extract_links).to eq(["http://www.example.org/foo",
+          "http://www.iana.org/"])
+      end
+    end
+
+    context "when body contains absolute urls" do
+      let(:body_absolute_urls) { "<p><a href='/foo'>More</a></p>
+      <p>Follow <a href='/bar'>official here</a>.</p>" }
+      subject(:parser_absolute_urls) { described_class.new(seed_url, body_absolute_urls) }
+      it "returns full url" do
+        expect(parser_absolute_urls.extract_links).to eq([
+          "http://www.example.org/foo", "http://www.example.org/bar"])
+      end
+    end
+  end
+end
