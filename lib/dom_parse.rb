62fff48a2bf9f52212d2b2b4bc29bcc68b538403
Dillan Pisavadia <dpisavadia20@gmail.com>
2017-03-07 15:42:31 +0000

    Function to extract links completed

--- lib/dom_parse.rb
+++ lib/dom_parse.rb
@@ -1,13 +1,23 @@
 require "nokogiri"
 
 class DomParse
-  attr_reader :body
+  attr_reader :seed_url, :body
 
-  def initialize(body)
+  def initialize(seed_url, body)
+    @seed_url = seed_url
     @body = body
   end
 
   def extract_links
-    
+    html = Nokogiri::HTML(body)
+    html.css("a").map do |link|
+      absolute_url(link["href"])
+    end
+  end
+
+  private
+
+  def absolute_url(link)
+    return link.include?("http") ? link : seed_url + link
   end
 end
