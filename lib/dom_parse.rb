14986f9212146fee53c0b6ec5b284ef5a51b8719
Dillan Pisavadia <dpisavadia20@gmail.com>
2017-03-07 16:27:54 +0000

    Completed function to extract assets

--- lib/dom_parse.rb
+++ lib/dom_parse.rb
@@ -1,4 +1,5 @@
 require "nokogiri"
+require "uri"
 
 class DomParse
   attr_reader :seed_url, :body
@@ -15,9 +16,29 @@ class DomParse
     end
   end
 
+  def extract_assets
+    html = Nokogiri::HTML(body)
+    selectors = ["img", "script", "link"]
+    assets = []
+    selectors.each do |selector|
+      html.css(selector).map do |asset|
+        assets << asset_type(selector, asset)
+      end
+    end
+    assets.uniq
+  end
+
   private
 
   def absolute_url(link)
     return link.include?("http") ? link : seed_url + link
+  end
+
+  def asset_type(selector, asset)
+    if selector == "link"
+      return asset["href"] if asset["href"].include?(".css")
+    else
+      return asset["src"]
+    end
   end
 end
