1f9b1d0a9b8abac1d5f34506a760a6f0bb0757b7
Dillan Pisavadia <dpisavadia20@gmail.com>
2017-03-21 11:01:24 +0000

    Completed crawler

---
+++ lib/crawler.rb
@@ -0,0 +1,58 @@
+require "set"
+require_relative "dom_parse"
+require_relative "http_request"
+require "uri"
+require "json"
+
+class Crawler
+  attr_reader :start_url, :stdout
+
+  def initialize(url)
+    @start_url = url
+    @visited_urls = Set.new
+    @stdout = []
+  end
+
+  def crawl
+    queue = [start_url]
+    until queue.empty?
+      p url = relative_url(queue.shift)
+      next if domain(url) || @visited_urls.include?(url)
+      @visited_urls << url
+      page = HTTPRequest.new(url).get
+      parse = DomParse.new(page)
+      links = parse.extract_links
+      links.map { |link| queue << link unless @visited_urls.include?(link) }
+      assets = parse.extract_assets
+      stdout_add(url, assets)
+    end
+  end
+
+  def stdout_add(url, assets)
+    @stdout << {url: url, assets: assets}
+  end
+
+  private
+
+  def relative_url(url)
+    return url[0] == "/" ? start_url + url : url
+  end
+
+  def domain(url)
+    begin
+      start_host = URI.parse(start_url).host
+      link_host = URI.parse(url).host
+      return start_host != link_host ? true : false
+    rescue
+      return true
+    end
+  end
+end
+
+domain = ARGV.first
+
+def crawler_run(domain)
+  crawler = Crawler.new(domain)
+  crawler.crawl
+  puts crawler.stdout.to_json
+end
