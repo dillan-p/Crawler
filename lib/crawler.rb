6a99f33b1d48aa1678929a37193b7d1e7254260a
dillan-p <dpisavadia20@gmail.com>
2018-07-09 23:23:27 +0100

    refactor variable and method names in crawler

--- lib/crawler.rb
+++ lib/crawler.rb
@@ -5,47 +5,44 @@ require "uri"
 require "json"
 
 class Crawler
-  attr_reader :start_url, :stdout
+  attr_reader :start_url, :url_assets
 
   def initialize(url)
     @start_url = url
     @visited_urls = Set.new
-    @stdout = []
+    @url_assets = []
   end
 
   def crawl
     queue = [start_url]
     until queue.empty?
-      p url = relative_url(queue.shift)
-      next if domain(url) || @visited_urls.include?(url)
-      @visited_urls << url
-      page = HTTPRequest.new(url).get
+      p current_url = format_url(queue.shift)
+      next if is_different_domain?(current_url) || @visited_urls.include?(current_url)
+      @visited_urls << current_url
+      page = HTTPRequest.new(current_url).get
       parse = DomParse.new(page)
       links = parse.extract_links
       links.map { |link| queue << link unless @visited_urls.include?(link) }
       assets = parse.extract_assets
-      stdout_add(url, assets)
+      store_url_assets(current_url, assets)
     end
-  end
-
-  def stdout_add(url, assets)
-    @stdout << {url: url, assets: assets}
   end
 
   private
 
-  def relative_url(url)
-    return url[0] == "/" ? start_url + url : url
+  def store_url_assets(url, assets)
+    @url_assets << {url: url, assets: assets}
   end
 
-  def domain(url)
-    begin
-      start_host = URI.parse(start_url).host
-      link_host = URI.parse(url).host
-      return start_host != link_host ? true : false
-    rescue
-      return true
-    end
+  def format_url(url)
+    escaped_url = URI.escape(url)
+    url[0] == "/" ? start_url + escaped_url : escaped_url
+  end
+
+  def is_different_domain?(url)
+    start_host = URI.parse(start_url).host
+    link_host = URI.parse(url).host
+    start_host != link_host ? true : false
   end
 end
 
@@ -54,7 +51,7 @@ domain = ARGV.first
 def crawler_run(domain)
   crawler = Crawler.new(domain)
   crawler.crawl
-  puts crawler.stdout.to_json
+  puts crawler.url_assets.to_json
 end
 
 crawler_run(domain)
