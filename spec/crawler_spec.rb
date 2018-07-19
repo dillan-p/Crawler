0e2bb22efd329a84c23c49c8406b8c5279ed75e2
dillan-p <dpisavadia20@gmail.com>
2018-07-19 22:34:27 +0100

    refactor crawler spec to use let variable for url

--- spec/crawler_spec.rb
+++ spec/crawler_spec.rb
@@ -4,47 +4,48 @@ require 'spec_helper'
 require 'crawler'
 
 RSpec.describe Crawler do
-  let(:base_url) { "http://fakecrawl.com#{path}" }
-  subject(:result) { described_class.new(base_url).crawl }
+  let(:base_url) { 'http://fakecrawl.com' }
+  let(:start_url) { "#{base_url}/#{path}" }
+  subject(:result) { described_class.new(start_url).crawl }
 
   context 'when there are no assets or links' do
     let(:path) { '' }
     it 'returns only the start url with no assets' do
-      is_expected.to eq([{ url: 'http://fakecrawl.com', assets: [] }])
+      is_expected.to eq([{ url: start_url, assets: [] }])
     end
   end
 
   context 'when there are links' do
-    let(:path) { '/links' }
+    let(:path) { 'links' }
     it 'returns them' do
       is_expected.to eq(
         [
-          { url: 'http://fakecrawl.com/links', assets: [] },
-          { url: 'http://fakecrawl.com/end', assets: [] }
+          { url: start_url, assets: [] },
+          { url: "#{base_url}/end", assets: [] }
         ]
       )
     end
   end
 
   context 'when there are assets' do
-    let(:path) { '/assets/1' }
+    let(:path) { 'assets/1' }
     it 'returns them and matches them with the corresponding url' do
       is_expected.to eq(
         [
-          { url: 'http://fakecrawl.com/assets/1', assets: ['theme.css'] },
-          { url: 'http://fakecrawl.com/assets/2', assets: ['test.png', 'index.js'] }
+          { url: start_url, assets: ['theme.css'] },
+          { url: "#{base_url}/assets/2", assets: ['test.png', 'index.js'] }
         ]
       )
     end
   end
 
   context 'when a request throws an HTTP error' do
-    let(:path) { '/error/200' }
+    let(:path) { 'error/200' }
     it 'is expected not to raise and the url is not recorded' do
       is_expected.to eq(
         [
-          { url: 'http://fakecrawl.com/error/200', assets: [] },
-          { url: 'http://fakecrawl.com/end', assets: [] }
+          { url: start_url, assets: [] },
+          { url: "#{base_url}/end", assets: [] }
         ]
       )
     end
